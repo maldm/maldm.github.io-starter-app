59dec934d10d0a65edd20cc0408737c3785f6e3b
Brett A. Lewis <brett@pendo.io>
2020-02-13 15:14:25 -0500

    attempt to jwt

--- src/app.js
+++ src/app.js
@@ -48,16 +48,6 @@ const jwt = JWT.sign({
   }
 }, 'f6c0d63e-22ab-4f57-7bd7-6d8c3b0907c6');
 
-(function(apiKey){
-  (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
-    v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
-      o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-    y=e.createElement(n);y.async=!0;y.src='https://pendo-apollo-static.storage.googleapis.com/agent/static/'+apiKey+'/pendo.js';
-    z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo')
-  console.log('hello again')
-
-})('7397188a-9b85-4825-5476-20de4cd4bfbe');
-
 pendo.initialize({
 
   jwt: jwt,
